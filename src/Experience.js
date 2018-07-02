e3ce8ec0fd134a00066b80ca48061d8139bd6fdd
unknown <joshmowens70@gmail.com>
2018-07-02 15:39:48 -0700

    link change

--- src/Experience.js
+++ src/Experience.js
@@ -18,7 +18,7 @@ const Experience = () =>{
     <div className="card-body">
       <h5 className="card-title">Encore</h5>
       <p className="card-text">A General contracting company where I would help build cabinets and redesign areas.</p>
-     
+      <p class="card-text"><small class="text-muted">8/25/2015 – 9/4/2016</small></p>
     </div>
   </div>
   <div className="card">
@@ -26,7 +26,7 @@ const Experience = () =>{
     <div className="card-body">
       <h5 className="card-title">Target</h5>
       <p className="card-text">While working at target I was part of the flexible fulfilment center. I would find, package, then ship out different items from the store. </p>
-      
+      <p class="card-text"><small class="text-muted">11/12/2016 – 1/10/2018</small></p>
     </div>
   </div>
   <div className="card">
@@ -34,7 +34,7 @@ const Experience = () =>{
     <div className="card-body">
       <h5 className="card-title">Lyft</h5>
       <p className="card-text">Ride-sharing company that evaluated your performance based on the customers input.</p>
-    
+      <p class="card-text align-bottom"><small class="text-muted">1/10/2018 - Now</small></p>
     </div>
   </div>
 </div>
--- src/Footer.js
+++ src/Footer.js
@@ -4,11 +4,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Foot = () =>{
     return(
-<div className="container pt-5">
-    <div className="row  pt-5 text-center">
+<div className="container mx-0 px-0">
+    <div className="row  pt-5 text-left">
     <hr/>
     <div className="col">
-    <button type="button" className="btn btn-link text-center" href="https://test-game-demo.herokuapp.com/" >Check out the progress I've made on my game!</button>
+    <a type="button" className="btn btn-link" href="https://test-game-demo.herokuapp.com/" >Check out the progress I've made on my game!</a>
     </div>
     </div>
 </div>    
